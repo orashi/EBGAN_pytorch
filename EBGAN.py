2bbadeb55b4083f337e9b832a7c03f831292e857
orashi <yuanzheng.ci@gmail.com>
2017-03-06 06:44:23 +0800

    more ebgan like

--- EBGAN.py
+++ EBGAN.py
@@ -21,12 +21,12 @@ parser.add_argument('--workers', type=int, help='number of data loading workers'
 parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
 parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
 parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
-parser.add_argument('--ngf', type=int, default=64)
-parser.add_argument('--ndf', type=int, default=64)
+parser.add_argument('--ngf', type=int, default=96)
+parser.add_argument('--ndf', type=int, default=96)
 parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
-parser.add_argument('--cuda'  , action='store_true', help='enables cuda')
+parser.add_argument('--cuda', action='store_true', help='enables cuda')
 parser.add_argument('--netG', default='', help="path to netG (to continue training)")
 parser.add_argument('--netD', default='', help="path to netD (to continue training)")
 parser.add_argument('--outf', default='.', help='folder to output images and model checkpoints')
@@ -66,17 +66,15 @@ ngf = int(opt.ngf)
 ndf = int(opt.ndf)
 nc = 3
 
+
 # custom weights initialization called on netG and netD
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
-        # m.weight.data.normal_(0.0, 0.02)   terrible
-        n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels            # He et al. 2015  Xavier/2
-        m.weight.data.normal_(0, math.sqrt(2. / n))
+        m.weight.data.normal_(0.0, 0.002)
     elif classname.find('BatchNorm') != -1:
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
-
 
 class _netG(nn.Module):
     def __init__(self):
@@ -94,12 +92,12 @@ class _netG(nn.Module):
         self.bn3 = nn.BatchNorm2d(ngf * 2)
         # state size. (ngf*2) x 16 x 16
 
-        self.convT4 = nn.ConvTranspose2d(ngf * 2, ngf, 5, 3, 1, bias=False)  # for 96*96 stride = 3
+        self.convT4 = nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False)
         self.bn4 = nn.BatchNorm2d(ngf)
         # state size. (ngf) x 32 x 32
 
-        self.convT5 = nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False)
-        # state size. (nc) x 64 x 64
+        self.convT5 = nn.ConvTranspose2d(ngf, nc, 5, 3, 1, bias=False)
+        # state size. (nc) x 96 x 96
 
     def forward(self, x):
 
@@ -120,35 +118,52 @@ print(netG)
 class _netD(nn.Module):
     def __init__(self):
         super(_netD, self).__init__()
-        # input is (nc) x 64 x 64   for anime it's 96x96
-        self.conv1 = nn.Conv2d(nc, ndf, 4, 2, 1, bias=False)
-        nn.LeakyReLU(0.2, inplace=True)
+        # input is (nc) x 96 x 96 (for anime image)
+        self.enc_conv1 = nn.Conv2d(nc, ndf, 5, 3, 1, bias=False)
         # state size. (ndf) x 32 x 32
 
-        self.conv2 = nn.Conv2d(ndf, ndf * 2, 5, 3, 1, bias=False)  # 3 stride for 96
-        self.bn2 = nn.BatchNorm2d(ndf * 2)
-        nn.LeakyReLU(0.2, inplace=True)
+        self.enc_conv2 = nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False)  # 3 stride for 96
+        self.enc_bn2 = nn.BatchNorm2d(ndf * 2)
         # state size. (ndf*2) x 16 x 16
 
-        self.conv3 = nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False)
-        self.bn3 = nn.BatchNorm2d(ndf * 4)
-        nn.LeakyReLU(0.2, inplace=True)
+        self.enc_conv3 = nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False)
+        self.enc_bn3 = nn.BatchNorm2d(ndf * 4)
         # state size. (ndf*4) x 8 x 8
 
-        self.conv4 = nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False)
-        self.bn4 = nn.BatchNorm2d(ndf * 8)
-        nn.LeakyReLU(0.2, inplace=True)
+        self.enc_conv4 = nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False)
+        self.enc_bn4 = nn.BatchNorm2d(ndf * 8)
         # state size. (ndf*8) x 4 x 4
 
-        self.conv5 = nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False)
+        self.dec_conv4 = nn.ConvTranspose2d(ndf * 8, ndf * 4, 4, 2, 1, bias=False)
+        self.dec_bn4 = nn.BatchNorm2d(ndf * 8)
+        # state size. (ndf*4) x 8 x 8
+
+        self.dec_conv3 = nn.ConvTranspose2d(ndf * 4, ndf * 2, 4, 2, 1, bias=False)
+        self.dec_bn3 = nn.BatchNorm2d(ndf * 8)
+        # state size. (ndf*2) x 16 x 16
+
+        self.dec_conv2 = nn.ConvTranspose2d(ndf * 2, ndf, 4, 2, 1, bias=False)
+        self.dec_bn2 = nn.BatchNorm2d(ndf * 8)
+        # state size. (ndf) x 32 x 32
+
+        self.dec_conv1 = nn.ConvTranspose2d(ndf, 3, 5, 3, 0, bias=False)
+        # state size. 3 x 96 x 96
+        ''' stride improvable '''
 
     def forward(self, x):
-        out = F.leaky_relu(self.conv1(x), 0.2, True)
-        out = F.leaky_relu(self.bn2(self.conv2(out)), 0.2, True)
-        out = F.leaky_relu(self.bn3(self.conv3(out)), 0.2, True)
-        out = F.leaky_relu(self.bn4(self.conv4(out)), 0.2, True)
-        out = F.sigmoid(self.conv5(out))
-        return out.view(-1, 1)
+        out = F.leaky_relu(self.enc_conv1(x), 0.2, True)
+        out = F.leaky_relu(self.enc_bn2(self.enc_conv2(out)), 0.2, True)
+        out = F.leaky_relu(self.enc_bn3(self.enc_conv3(out)), 0.2, True)
+        out = F.leaky_relu(self.enc_bn4(self.enc_conv4(out)), 0.2, True)
+
+        ''' question: fc layer???? '''
+
+        out = F.leaky_relu(self.dec_bn4(self.dec_conv4(out)), 0.2, True)
+        out = F.leaky_relu(self.dec_bn3(self.dec_conv3(out)), 0.2, True)
+        out = F.leaky_relu(self.dec_bn2(self.dec_conv2(out)), 0.2, True)
+        out = self.dec_conv1(out)
+
+        return out
 
 netD = _netD()
 netD.apply(weights_init)
