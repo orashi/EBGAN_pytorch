gradient bug fix

EBGAN.py
@@ -23,7 +23,7 @@ parser.add_argument('--imageSize', type=int, default=64, help='the height / widt
 parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
 parser.add_argument('--ngf', type=int, default=96)
 parser.add_argument('--ndf', type=int, default=96)
-parser.add_argument('--margin', type=float, default=80)
+parser.add_argument('--margin', type=float, default=80, help='margin of the energy loss')
 parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
@@ -159,7 +159,6 @@ class _netD(nn.Module):
         self.dec_conv1 = nn.ConvTranspose2d(ndf, 3, 5, 3, 1, bias=False)
         # state size. 3 x 96 x 96
         ''' stride improvable '''
-        ''' output padding? '''
 
         self.MSE = nn.MSELoss()
 
@@ -255,8 +254,8 @@ for epoch in range(opt.niter):
         netG.zero_grad()
 
         # reuse generated fake samples
-        output = netD(fake.detach())
-        errG = criterion_MSE(output, fake.detach())
+        output = netD(fake)
+        errG = (output - fake).pow(2).mean()  # MSE
         errG.backward()
         D_G_z2 = errG.data.mean()
 